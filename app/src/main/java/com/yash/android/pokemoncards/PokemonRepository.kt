b748b0126f49df6e75c9c2953a2cd78430b7eb5f
Yashwanth Gajji <yashwanth.gajji333@gmail.com>
2023-11-07 20:51:47 -0500

    taking dataset from csv file

--- app/src/main/java/com/yash/android/pokemoncards/PokemonRepository.kt
+++ app/src/main/java/com/yash/android/pokemoncards/PokemonRepository.kt
@@ -1,121 +1,18 @@
 package com.yash.android.pokemoncards
 
 import android.content.Context
+import android.util.Log
 import com.yash.android.pokemoncards.models.Pokemon
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import java.util.UUID
 
-class PokemonRepository private constructor(context: Context) {
+const val TAG = "PokemonRepository"
+class PokemonRepository private constructor(private val context: Context) {
     private var pokemons = mutableListOf<Pokemon>()
 
     init {
-        pokemons = mutableListOf(
-            Pokemon(
-                UUID.randomUUID(),
-                "Bulbasaur",
-                "bulbasaur.png",
-                0.7,
-                6.9,
-                45,
-                49,
-                49,
-                65,
-                65,
-                45
-            ),
-            Pokemon(
-                UUID.randomUUID(),
-                "Arbok",
-                "arbok.png",
-                3.5,
-                65.0,
-                60,
-                95,
-                69,
-                65,
-                79,
-                80
-            ),
-            Pokemon(
-                UUID.randomUUID(),
-                "Charmander",
-                "charmander.png",
-                0.6,
-                8.5,
-                39,
-                52,
-                43,
-                60,
-                50,
-                65
-            ),
-            Pokemon(
-                UUID.randomUUID(),
-                "Mewtwo",
-                "mewtwo.png",
-                2.0,
-                122.0,
-                106,
-                110,
-                90,
-                154,
-                90,
-                130
-            ),
-            Pokemon(
-                UUID.randomUUID(),
-                "Moltres",
-                "moltres.png",
-                2.0,
-                60.0,
-                90,
-                100,
-                90,
-                125,
-                85,
-                90
-            ),
-            Pokemon(
-                UUID.randomUUID(),
-                "Onix",
-                "onix.png",
-                8.8,
-                210.0,
-                35,
-                45,
-                160,
-                30,
-                45,
-                70
-            ),
-            Pokemon(
-                UUID.randomUUID(),
-                "Pikachu",
-                "pikachu.png",
-                0.4,
-                6.0,
-                35,
-                55,
-                40,
-                50,
-                50,
-                90
-            ),
-            Pokemon(
-                UUID.randomUUID(),
-                "Treecko",
-                "treecko.png",
-                0.5,
-                5.0,
-                40,
-                45,
-                35,
-                65,
-                55,
-                70
-            )
-        )
+        pokemons = readDataSet()
     }
 
     fun getAllPokemons(): Flow<List<Pokemon>> {
@@ -124,6 +21,49 @@ class PokemonRepository private constructor(context: Context) {
 
     fun getPokemonById(pokemonId: UUID): Flow<Pokemon?> {
         return flowOf(pokemons.find { pokemon -> pokemon.id == pokemonId })
+    }
+
+    private fun readDataSet(): MutableList<Pokemon> {
+        val assetManager = context.assets
+        val pokemonList = mutableListOf<Pokemon>()
+        try {
+            val inputStream = assetManager.open("pokedex.csv")
+            inputStream.bufferedReader().useLines { lines ->
+                var i = 0
+                for (line in lines) {
+                    if (i == 0) {
+                        i++
+                        continue
+                    }
+                    val values = line.split(",")
+                    val logoResourceId = context.resources.getIdentifier(
+                        values[2].lowercase(),
+                        "drawable",
+                        context.packageName
+                    )
+                    if (logoResourceId != 0) {
+                        val currPokemon = Pokemon(
+                            UUID.randomUUID(),
+                            values[2],
+                            values[2].lowercase() + ".png",
+                            values[11].toDouble(),
+                            values[12].toDouble(),
+                            values[18].toDouble().toInt(),
+                            values[19].toDouble().toInt(),
+                            values[20].toDouble().toInt(),
+                            values[21].toDouble().toInt(),
+                            values[22].toDouble().toInt(),
+                            values[23].toDouble().toInt()
+                        )
+                        Log.d(TAG, "$currPokemon")
+                        pokemonList.add(currPokemon)
+                    }
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return pokemonList
     }
 
     companion object {
